a2e98f50f2b62c1cc24f72f920a687fe30c7ef1c
Armin Hosseini <armin685@gmail.com>
2022-05-31 11:02:13 +0430

    eliminate maximum node problem

--- Graph.py
+++ Graph.py
@@ -1,23 +1,36 @@
-# This code is contributed by Neelam Yadav and complete instruction is available on 
+# This code is contributed by Neelam Yadav and complete instruction is available on
 # https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/
 
 
 # pyhton code to print BFS traversal from a given source.
 from collections import defaultdict
+import numpy as np
+
 
 class Graph:
 
     def __init__(self):
         self.graph = defaultdict(list)
 
-
     # function to add an edge to graph
+
     def addEdge(self, u, v):
         self.graph[u].append(v)
 
+    # find the maximum number of nodes
+
+    def maxNode(self):
+
+        nodes = list(self.graph.values())
+        nodes.append(list(self.graph.keys()))
+        nodes = list(np.concatenate(nodes).flat)
+        uniqueNodes = np.unique(nodes)
+        return np.max(uniqueNodes)
+
     # Function to print a BFS of graph
+
     def BFS(self, s):
-        visited = [False] * (max(self.graph) + 1)
+        visited = [False] * (self.maxNode() + 1)
         queue = []
         queue.append(s)
         visited[s] = True
@@ -35,9 +48,9 @@ class Graph:
 g = Graph()
 g.addEdge(0, 1)
 g.addEdge(0, 2)
-g.addEdge(1, 2)
-g.addEdge(2, 0)
-g.addEdge(2, 3)
-g.addEdge(3, 3)
- 
-g.BFS(2)
+g.addEdge(1, 3)
+g.addEdge(1, 4)
+g.addEdge(2, 5)
+g.addEdge(2, 6)
+
+g.BFS(0)
